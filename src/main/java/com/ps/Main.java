bf95499c680566a5c7735328481c77a417d47f66
Marshall <mellisgibbs@gmail.com>
2024-04-19 11:10:41 -0400

    Created Book class & some Pseudocode

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,4 +1,17 @@
 package com.ps;
 
 public class Main {
+    public static void main(String[] args) {
+
+        // Store Home Screen
+
+            // Show Available books
+
+            // Show Checked Out books
+
+                // Check-in book
+
+
+
+    }
 }
